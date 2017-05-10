5d9d8bcd11305ace141bc39ebf87192fde0f98f5
Ominous Gremlin <ominousgremlin@gmail.com>
2017-05-10 23:08:20 +0200

    question section js improve

--- js/action.js
+++ js/action.js
@@ -3,14 +3,15 @@ window.addEventListener('load', function(){
     var listButtons = document.getElementsByClassName('main__list-question-element');
     var articles = document.getElementsByClassName('main__question-article');
     function toggleButton(element) {
+        var activeButton = document.getElementsByClassName('main__list-question-element_active');
         var articleVisible = document.getElementsByClassName('main__question-article_visible');
         if (element.getAttribute("aria-pressed") === "false") {
-            for(var i=0;i<listButtons.length;i++){
-                listButtons[i].setAttribute("aria-pressed", "false");
-                listButtons[i].setAttribute("aria-expanded", "false");
-            }
+            activeButton[0].setAttribute("aria-pressed", "false");
+            activeButton[0].setAttribute("aria-expanded", "false");
+            activeButton[0].classList.toggle('main__list-question-element_active');
             element.setAttribute("aria-pressed", "true");
             element.setAttribute("aria-expanded", "true");
+            element.classList.add('main__list-question-element_active');
             for (var j = 0; j < listButtons.length; j++) {
                 if (listButtons[j].getAttribute("aria-pressed") === element.getAttribute("aria-pressed") ) {
                     articleVisible[0].classList.toggle('main__question-article_hidden');
