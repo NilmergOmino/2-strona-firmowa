62eef04ce38c06ea40596fa79bbdc388e14fedec
Ominous Gremlin <ominousgremlin@gmail.com>
2017-05-12 13:32:57 +0200

    smooth scrolling improve

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -1,35 +1,32 @@
 window.addEventListener('load', function(){
-
-var smoothScrolling = function(anchorId,anchorTarget){
-    var anchor = document.getElementById(anchorId);
-        target = document.getElementById(anchorTarget);
-    anchor.onclick = function(){
-        var from = pageYOffset;
-            upto = target.offsetTop;
-        var scroll = function(){
-            if(pageYOffset < upto) var step = Math.floor((upto - pageYOffset)*3/100);
-            else if(pageYOffset > upto) var step = Math.floor((pageYOffset - upto)*3/100);
-            if (step < 2) step = 2;
-            if (from < upto) {
-                window.scrollBy(0, step);
-                from += step;
-                window.requestAnimationFrame(scroll);
+    var navElements = document.getElementsByClassName('nav__link');
+    for(var i=0;i<navElements.length; i++){
+        var navElement = navElements[i];
+        navElement.onclick = function(){
+            var anchor = this.hash;
+            var target = document.getElementById(this.hash.slice(1));
+                from = pageYOffset;
+                upto = target.offsetTop;
+            var scroll = function(){
+                if(pageYOffset < upto) var step = Math.floor((upto - pageYOffset)*4/100);
+                else if(pageYOffset > upto) var step = Math.floor((pageYOffset - upto)*4/100);
+                if (step < 3) step = 3;
+                if (from < upto) {
+                    window.scrollBy(0, step);
+                    from += step;
+                    window.requestAnimationFrame(scroll);
+                }
+                else if(from > upto+1) {
+                    window.scrollBy(0, -step);
+                    from -= step;
+                    window.requestAnimationFrame(scroll);
+                }
+                if(from <= upto +10 && from >= upto-10 ){
+                    location.hash = anchor;
+                }
             }
-            else if(from > upto+9) {
-                window.scrollBy(0, -step);
-                from -= step;
-                window.requestAnimationFrame(scroll);
-            }
-            if(from <= upto +10 && from >= upto-10 ){
-                location.hash = anchor.hash;
-            }
+            window.requestAnimationFrame(scroll);
+            return false;
         }
-        window.requestAnimationFrame(scroll);
-        return false;
     }
-}
-smoothScrolling('o-nas_anchor','o-nas');
-smoothScrolling('podroze_anchor','podroze');
-smoothScrolling('pytania_anchor','pytania-i-odpowiedzi');
-smoothScrolling('kontakt_anchor','kontakt');
 })
--- js/sticky-nav.js
+++ js/sticky-nav.js
@@ -1,5 +1,8 @@
     var nav = document.getElementById('nav');
         topPos = nav.offsetTop;
+window.addEventListener('load', function(){
+    if(pageYOffset >= topPos) nav.classList.add('nav_sticky');
+})
 window.addEventListener("scroll", function(){
     var currentPos = pageYOffset;
     if(currentPos >= topPos){
