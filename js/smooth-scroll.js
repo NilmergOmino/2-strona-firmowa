e99adb5618d15e70220409cc3b3442657f39de02
Ominous Gremlin <ominousgremlin@gmail.com>
2017-05-13 18:56:39 +0200

    some cosmetic changes

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -3,13 +3,14 @@ window.addEventListener('load', function(){
     for(var i=0;i<navElements.length; i++){
         var navElement = navElements[i];
         navElement.onclick = function(){
-            var anchor = this.hash;
-            var target = document.getElementById(this.hash.slice(1));
+            var check = true;
+                anchor = this.hash;
+                target = document.getElementById(this.hash.slice(1));
                 from = pageYOffset;
                 upto = target.offsetTop;
             var scroll = function(){
-                if(pageYOffset < upto) var step = Math.floor((upto - pageYOffset)*4/100);
-                else if(pageYOffset > upto) var step = Math.floor((pageYOffset - upto)*4/100);
+                if(pageYOffset < upto ) var step = Math.floor((upto - pageYOffset)*6/100);
+                else if(pageYOffset > upto) var step = Math.floor((pageYOffset - upto)*6/100);
                 if (step < 1) step = 1;
                 if (from < upto) {
                     window.scrollBy(0, step);
@@ -21,8 +22,9 @@ window.addEventListener('load', function(){
                     from -= step;
                     window.requestAnimationFrame(scroll);
                 }
-                if(from == upto){
-                    location.hash = anchor;
+                if(step < 10 && check==true){
+                    history.pushState(null,null,anchor);
+                    check = false;
                 }
             }
             window.requestAnimationFrame(scroll);
